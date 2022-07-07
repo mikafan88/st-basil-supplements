044afe5c694c8a9bf016c0a8e6d9ab74278ee796
mikafan88 <89216133+mikafan88@users.noreply.github.com>
2022-07-07 16:36:21 -0400

    Add files via upload

---
+++ clusterText.py
@@ -0,0 +1,37 @@
+
+
+def cluster_text(text):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    vectorizer = TfidfVectorizer(stop_words={'english'})
+    X = vectorizer.fit_transform(text)
+
+    import matplotlib.pyplot as plt
+    from sklearn.cluster import KMeans
+    Sum_of_squared_distances = []
+    K = range(2,10)
+    for k in K:
+       km = KMeans(n_clusters=k, max_iter=200, n_init=10)
+       km = km.fit(X)
+       Sum_of_squared_distances.append(km.inertia_)
+    plt.plot(K, Sum_of_squared_distances, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Sum_of_squared_distances')
+    plt.title('Elbow Method For Optimal k')
+    plt.show()
+
+    print('How many clusters do you want to use?')
+    true_k = int(input())
+    model = KMeans(n_clusters=true_k, init='k-means++', max_iter=200, n_init=10)
+    model.fit(X)
+
+    labels=model.labels_
+    clusters=pd.DataFrame(list(zip(text,labels)),columns=['title','cluster'])
+    #print(clusters.sort_values(by=['cluster']))
+
+    for i in range(true_k):
+        print(clusters[clusters['cluster'] == i])
+        
+    return
+
+with open ("BasilMergeV2.txt", "r") as f:
+	cluster_text(f)
